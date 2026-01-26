79581a25e92e3b57469d10073ec81fc79442e6b0
zhuxiaowei <zhuxiaowei@infini-ai.com>
2026-01-26 11:56:47 +0800

    fix user type

--- authentication/user/user.go
+++ authentication/user/user.go
@@ -2,25 +2,43 @@ package user
 
 import "encoding/json"
 
+type UserType string
+
+func (u UserType) String() string {
+	return string(u)
+}
+
+const (
+	UserTypeAccount        UserType = "account"
+	UserTypeUser           UserType = "user"
+	UserTypeAdmin          UserType = "admin"
+	UserTypeService        UserType = "service"
+	UserTypeServiceAccount UserType = "service_account"
+)
+
 type Info interface {
+	UserType() UserType
 	GetName() string
 	GetID() string
 	GetDomain() string
 	GetEmail() string
 	GetPhone() string
 	GetGroups() []string
-	SetExtra(key string, values []string)
-	GetExtra(key string) []string
+	SetExtra(key string, value any)
+	GetExtra(key string) any
 }
 
+var _ Info = (*DefaultInfo)(nil)
+
 type DefaultInfo struct {
-	Name   string              `json:"name"`
-	ID     string              `json:"id"`
-	Domain string              `json:"domain"`
-	Email  string              `json:"email"`
-	Phone  string              `json:"phone"`
-	Groups []string            `json:"groups,omitempty"`
-	Extra  map[string][]string `json:"extra,omitempty"`
+	Type   UserType       `json:"type"`
+	Name   string         `json:"name"`
+	ID     string         `json:"id"`
+	Domain string         `json:"domain"`
+	Email  string         `json:"email"`
+	Phone  string         `json:"phone"`
+	Groups []string       `json:"groups,omitempty"`
+	Extra  map[string]any `json:"extra,omitempty"`
 }
 
 func (i DefaultInfo) MarshalBinary() ([]byte, error) {
@@ -29,6 +47,10 @@ func (i DefaultInfo) MarshalBinary() ([]byte, error) {
 
 func (i *DefaultInfo) UnmarshalBinary(data []byte) error {
 	return json.Unmarshal(data, i)
+}
+
+func (i *DefaultInfo) UserType() UserType {
+	return i.Type
 }
 
 func (i *DefaultInfo) GetName() string {
@@ -55,14 +77,14 @@ func (i *DefaultInfo) GetGroups() []string {
 	return i.Groups
 }
 
-func (i *DefaultInfo) SetExtra(key string, values []string) {
+func (i *DefaultInfo) SetExtra(key string, value any) {
 	if i.Extra == nil {
-		i.Extra = make(map[string][]string)
+		i.Extra = make(map[string]any)
 	}
-	i.Extra[key] = values
+	i.Extra[key] = value
 }
 
-func (i *DefaultInfo) GetExtra(key string) []string {
+func (i *DefaultInfo) GetExtra(key string) any {
 	if i.Extra == nil {
 		return nil
 	}
